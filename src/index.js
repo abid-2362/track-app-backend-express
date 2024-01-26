2d32f9c23d0762096a7e220d0b795d83b907b7e0
Abid Ali <abdsoftfsd@gmail.com>
2024-01-26 10:20:39 +0500

    Added route to get and post the tracks

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.get('/', authMiddleware, (req, res) => {
   res.send(`Hi ${req.user.email}`);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on ${PORT}`);
--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -7,8 +7,41 @@ router.use(autMiddleware);
 
 router.get('/', async (req, res) => {
   const { user } = req;
-  const tracks = await Track.find({ userId: user._id });
+  const tracks = await Track.find({ userId: user._id }, null, null)
+    .exec();
   res.send(tracks);
+});
+
+// eslint-disable-next-line consistent-return
+router.post('/', async (req, res) => {
+  const {
+    name,
+    locations
+  } = req.body;
+
+  if (!name || !locations) {
+    return res.status(422)
+      .send({ error: 'Please provide name and locations' });
+  }
+
+  try {
+    const track = new Track({
+      userId: req.user._id,
+      name,
+      locations
+    });
+    await track.save();
+    res.status(200)
+      .send({
+        message: 'Track Saved successfully',
+        error: false,
+        track
+      });
+  } catch (err) {
+    console.log(err.message); // eslint-disable-line no-console
+    res.status(422)
+      .send({ error: 'Something went wrong.' });
+  }
 });
 
 module.exports = router;
